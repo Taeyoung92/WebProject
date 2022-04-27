69aa7e1bd6f68df62924b6158810c83a25e58ed8
Taeyoung Lee <marine4391@naver.com>
2022-04-27 21:08:49 +0900

    add Recommendation

---
+++ project1st/src/main/java/com/shop/board/answer/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.shop.board.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
--- project1st/src/test/java/com/shop/board/Project1stApplicationTests.java
+++ project1st/src/test/java/com/shop/board/Project1stApplicationTests.java
@@ -1,13 +1,24 @@
 package com.shop.board;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.shop.board.question.QuestionService;
 
 @SpringBootTest
 class Project1stApplicationTests {
+	
+	@Autowired
+    private QuestionService questionService;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testJpa() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content, null);
+        }
+    }
 
 }
