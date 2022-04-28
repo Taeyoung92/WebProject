ed292c7106c84076f58e7850eb270b6c0a25eb39
Taeyoung Lee <marine4391@naver.com>
2022-04-28 22:39:43 +0900

    Add search function

--- project1st/src/main/java/com/shop/board/controller/MainController.java
+++ project1st/src/main/java/com/shop/board/controller/MainController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-	@RequestMapping("/sbb")
+	@RequestMapping("/board")
 	@ResponseBody
     public String index() {
-        return "안녕하세요 sbb에 오신것을 환영합니다.";
+        return "BOARD 프로젝트입니다.";
 	}
 	
 	@RequestMapping("/")
     public String root() {
-        return "redirect:/question/list";
+        return "index";
     }
 }
